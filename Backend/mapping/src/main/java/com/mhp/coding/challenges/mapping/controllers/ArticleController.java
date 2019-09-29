e0e35af84dd248ce2b09eecf3925e7dc606bde28
Sourceror <andre.dietl@outlook.com>
2019-09-29 23:16:11 +0200

    Removed unused imports.

--- Backend/mapping/src/main/java/com/mhp/coding/challenges/mapping/controllers/ArticleController.java
+++ Backend/mapping/src/main/java/com/mhp/coding/challenges/mapping/controllers/ArticleController.java
@@ -1,17 +1,14 @@
 package com.mhp.coding.challenges.mapping.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.mhp.coding.challenges.mapping.models.dto.ArticleDto;
 import com.mhp.coding.challenges.mapping.services.ArticleService;
